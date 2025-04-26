9def5729a5ab2665b6fc70f3ed872cbd543591ac
sandeep6268 <sandeepsinghkhicchi@gmail.com>
2025-04-26 18:02:35 +0530

    Updated payment handler and routes

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,12 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/',
+  build: {
+    rollupOptions: {
+      output: {
+        entryFileNames: `[name]-[hash].js`,  // Adds hash to filenames
+        assetFileNames: `[name]-[hash].[ext]`
+      }
+    }
+  }
 })
