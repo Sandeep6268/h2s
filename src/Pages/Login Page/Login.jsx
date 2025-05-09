ce7265c8be9922f34bbde0b2eee1cde130ec884d
sandeep6268 <sandeepsinghkhicchi@gmail.com>
2025-05-09 17:23:09 +0530

    Initial commit

--- src/Pages/Login Page/Login.jsx
+++ src/Pages/Login Page/Login.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import API from "../../api";
+import "./Login.css"; // Make sure to create this CSS file
 
 const Login = () => {
   const [data, setData] = useState({ email: "", password: "" });
@@ -10,13 +11,14 @@ const Login = () => {
     setData({ ...data, [e.target.name]: e.target.value });
   };
 
-  const handleLogin = async () => {
+  const handleLogin = async (e) => {
+    e.preventDefault();
     try {
       const res = await API.post("jwt/create/", data);
       localStorage.setItem("access", res.data.access);
       localStorage.setItem("refresh", res.data.refresh);
       alert("Login successful");
-      navigate("/"); // Redirect to home
+      navigate("/");
     } catch (err) {
       console.error(err.response?.data || err);
       alert("Login failed");
@@ -24,18 +26,44 @@ const Login = () => {
   };
 
   return (
-    <div>
-      <h2>Login</h2>
-      <input name="email" placeholder="Email" onChange={handleChange} />
-      <input
-        name="password"
-        placeholder="Password"
-        type="password"
-        onChange={handleChange}
-      />
-      <button onClick={handleLogin}>Login</button>
-      <Link to={'/register'}>Don't having account?</Link>
-    </div>
+    <section className="container pt-5">
+      <div className="login-container mx-auto">
+        <div className="circle circle-one"></div>
+        <div className="form-container">
+          <img
+            src="https://raw.githubusercontent.com/hicodersofficial/glassmorphism-login-form/master/assets/illustration.png"
+            alt="illustration"
+            className="illustration"
+          />
+          <h1 className="opacity">LOGIN</h1>
+          <form onSubmit={handleLogin}>
+            <input
+              type="email"
+              name="email"
+              placeholder="EMAIL"
+              onChange={handleChange}
+              value={data.email}
+              required
+            />
+            <input
+              type="password"
+              name="password"
+              placeholder="PASSWORD"
+              onChange={handleChange}
+              value={data.password}
+              required
+            />
+            <button type="submit" className="opacity">
+              SUBMIT
+            </button>
+          </form>
+          <div className="register-forget opacity">
+            <Link to="/register" className="text-decoration-none text-danger">Don't have an account?</Link>
+          </div>
+        </div>
+        <div className="circle circle-two"></div>
+      </div>
+    </section>
   );
 };
 
