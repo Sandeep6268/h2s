f1625d5da1e2b7b04db684d5cbbada612e93e436
sandeep6268 <sandeepsinghkhicchi@gmail.com>
2025-05-11 18:56:04 +0530

    Initial commit

--- src/Component/Header/Header.jsx
+++ src/Component/Header/Header.jsx
@@ -97,7 +97,10 @@ const Header = () => {
             {/* Modal Popup */}
             {showModal && (
               <div className="your-courses-modal">
-                <div className="modal-content" >
+                <div
+                  className="modal-content"
+                  style={{ top: "100% !important" }}
+                >
                   <h3>Your Enrolled Courses</h3>
                   <button
                     onClick={() => setShowModal(false)}
