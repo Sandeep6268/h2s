5dc93755842712094879dbcdeb67dca767116ea1
sandeep6268 <sandeepsinghkhicchi@gmail.com>
2025-05-11 20:21:43 +0530

    Initial commit

--- src/Component/Courses/Courses Page/HTML CSS/HtmlCss.jsx
+++ src/Component/Courses/Courses Page/HTML CSS/HtmlCss.jsx
@@ -101,6 +101,25 @@ const HtmlCss = () => {
     <>
       {" "}
       <Header />{" "}
+      <div className="floating-certificate-btn">
+        <button 
+          className="certificate-cta pulse-animation"
+          onClick={() => setShowForm(true)}
+        >
+          <span className="certificate-icon">
+            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
+              <path d="M14.5 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V7.5L14.5 2z"></path>
+              <polyline points="14 2 14 8 20 8"></polyline>
+              <path d="M10 13h4"></path>
+              <path d="M10 17h4"></path>
+              <path d="M8 13h1"></path>
+              <path d="M8 17h1"></path>
+            </svg>
+          </span>
+          <span className="cta-text">Get Your Certificate</span>
+          <span className="badge">FREE</span>
+        </button>
+      </div>
       <div className="container-fluid my-2 main-yt-div row align-content-center margin-top">
         {" "}
         <div className="col-md-9 yt-video-div">
@@ -167,12 +186,12 @@ const HtmlCss = () => {
             ))}
           </div>
         </div>
-        <button
+        {/* <button
           className="btn btn-info w-25 mx-auto"
           onClick={() => setShowForm(true)}
         >
           Get your certificate
-        </button>
+        </button> */}
       </div>
       {showForm && (
         <div
