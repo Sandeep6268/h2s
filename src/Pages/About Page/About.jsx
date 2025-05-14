b5271ea6dead192c6c89140a93672a18fa9dfad0
sandeep6268 <sandeepsinghkhicchi@gmail.com>
2025-05-14 15:33:21 +0530

    Initial commit

--- src/Pages/About Page/About.jsx
+++ src/Pages/About Page/About.jsx
@@ -108,9 +108,9 @@ const About = () => {
                     className="img-fluid w-100 zoom-hover"
                   />
                   <div className="portfolio-overlay position-absolute w-100 h-100 d-flex align-items-center justify-content-center">
-                    <Button variant="outline-light" size="sm">
+                    {/* <Button variant="outline-light" size="sm">
                       View Project
-                    </Button>
+                    </Button> */}
                   </div>
                 </div>
               </Col>
