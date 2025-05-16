123f63b070230d5edb45e8fe7667e49a9d84033d
sandeep6268 <sandeepsinghkhicchi@gmail.com>
2025-05-16 23:56:23 +0530

    Initial commit

--- src/Component/Courses/Courses Page/CoursePage.jsx
+++ src/Component/Courses/Courses Page/CoursePage.jsx
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import "./CoursePage.css";
 import { Context } from "../../../Context";
 import banner2 from "../../../images/banner2.jpeg";
