fd7688e337d1b02bcf09594590e17e7c9ae3cf57
sandeep6268 <sandeepsinghkhicchi@gmail.com>
2025-05-18 23:11:23 +0530

    Initial commit

--- src/Component/HeroSection/Hero.jsx
+++ src/Component/HeroSection/Hero.jsx
@@ -5,20 +5,27 @@ import video from "../../images/video.mp4";
 const Hero = () => {
   return (
     <>
-      <section className="container d-sm-block d-md-none" style={{marginTop:'115px'}}>
+      <section
+        className="container d-sm-block d-md-none"
+        style={{ marginTop: "115px" }}
+      >
         <video src={video} className="w-100" autoPlay loop muted></video>
       </section>
-      <section className="banner container-fluid d-sm-none d-md-block" style={{marginTop:'115px'}}>
-        <h1 className="text-center py-3" data-aos='zoom-in'>
+      <section
+        className="banner container-fluid d-sm-none d-md-block"
+        style={{ marginTop: "115px" }}
+      >
+        <h1 className="text-center py-3" data-aos="zoom-in">
           CODE | <span className="text-primary">CREATE</span> | CONQUER
         </h1>
         <div className="hero-container row w-100 p-2 py-md-5">
-          <div className=" col-md-3 offset-md-1" data-aos='fade-up'>
+          <div className=" col-md-3 offset-md-1" data-aos="fade-up">
             <img src={heroimg} className="w-100" alt="" />
           </div>
-          <div className=" col-md-5  offset-md-1" data-aos='fade-down'>
+          <div className=" col-md-5  offset-md-1" data-aos="fade-down">
             <h2 className="font-change">
-              We deliver smart tech solutions tailored for modern businesses.
+              We’re not just coders – we’re creators. H2S Tech delivers smart,
+              scalable, and future-ready tech solutions.
             </h2>
           </div>
         </div>
